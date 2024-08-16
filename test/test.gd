9c98b223d540430995c5cc1bfc9dcd1779d820ac
Cael Rowley <drdeus@pm.me>
2024-08-16 17:42:00 +0200

    feat: add scene manager with fade out/in

--- test/test.gd
+++ test/test.gd
@@ -7,3 +7,10 @@ func _ready() -> void:
 	AudioManager.play_sfx(AudioManager.sfx_accent_boing)
 	AudioManager.play_ui(AudioManager.sfx_achievement_01)
 	AudioManager.play_from_file("res://audio/sfx/khorn_studio/Puzzle_Game_Merge_Echo_03.wav", AudioManager.AUDIO_BUSES.Master)
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept"):
+		SceneManager.goto_scene("res://test/test2.tscn")
+	if event.is_action_pressed("ui_cancel"):
+		SceneManager.goto_previous_scene()
