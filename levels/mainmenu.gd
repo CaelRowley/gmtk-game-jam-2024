604dbf57bee1519298638985706989ea07c66fc7
LukeNelson <luke@lukenelson.info>
2024-08-17 19:57:41 +0200

    feat: created main menu and how to play menus

---
+++ levels/mainmenu.gd
@@ -0,0 +1,27 @@
+extends Control
+
+
+
+
+func _on_start_game_pressed() -> void:
+	print("Im starting")
+
+
+func _on_endless_mode_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_how_to_play_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_options_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_start_game_mouse_entered() -> void:
+	print("Im starting")
